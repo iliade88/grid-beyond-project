3e357fdb59b44ed33c494b21799ae68ec4e1e336
Cristobal <crisjg10@gmail.com>
2021-01-10 17:37:55 +0100

    Renamed Home and created upload file feature

---
+++ GridBeyondProject/GridBeyondProject/Controllers/UploadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GridBeyondProject.Controllers
+{
+    public class UploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = TempData["SaveFileMessage"];
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveFile(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/UploadedFiles"),
+                                               Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    TempData["SaveFileMessage"] = "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    TempData["SaveFileMessage"] = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                TempData["SaveFileMessage"] = "You have not specified a file.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
