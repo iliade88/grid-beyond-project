549c1b3b13ef38b53df218725c5c4df3bca89573
Cristobal <crisjg10@gmail.com>
2021-01-10 16:52:32 +0100

    Created project

---
+++ GridBeyondProject/GridBeyondProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GridBeyondProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
